57c77579fef59ef811a0e45f53e2d6a430bc92cd
Chuong Tran <chuong.tran@tiny.fun>
2023-10-24 13:32:33 +0700

    first commit

---
+++ lib/models/weather_data.dart
@@ -0,0 +1,16 @@
+
+import 'weather_data_current.dart';
+import 'weather_data_daily.dart';
+import 'weather_data_hourly.dart';
+
+class WeatherData {
+  final WeatherDataCurrent? current;
+  final WeatherDataHourly? hourly;
+  final WeatherDataDaily? daily;
+
+  WeatherData([this.current, this.hourly, this.daily]);
+
+  WeatherDataCurrent getCurrentWeather() => current!;
+  WeatherDataHourly getHourlyWeather() => hourly!;
+  WeatherDataDaily getDailyWeather() => daily!;
+}
